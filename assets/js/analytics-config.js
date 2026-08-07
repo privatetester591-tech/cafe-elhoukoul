a962b8e5551595feae9b334db85d57d7e3ae10ef
privatetester591-tech <privatetester591@gmail.com>
2026-08-07 20:22:09 +0100

    Update analytics-config.js

--- assets/js/analytics-config.js
+++ assets/js/analytics-config.js
@@ -6,5 +6,5 @@
 
 const AnalyticsConfig = {
   endpoint:
-    "https://script.google.com/macros/s/AKfycbx6nM8mYavs9h3FQo9no-9GvfLtc6L3Na24waMMyBw/dev",
+    "https://script.google.com/macros/s/AKfycbx6nM8mYavs9h3FQo9no-9GvfLtc6L3Na24waMMyBw/exec",
 };
